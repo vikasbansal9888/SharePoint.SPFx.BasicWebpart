ec2c3377e84f0ce4bfca5c961f8525b89f1f6622
vikasbansal9888 <vikasbansal9888@gmail.com>
2019-03-03 19:42:36 +0530

    VB: HelloBasicWorld webpart.

---
+++ src/webparts/helloBasicWorld/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IHelloBasicWorldWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'HelloBasicWorldWebPartStrings' {
+  const strings: IHelloBasicWorldWebPartStrings;
+  export = strings;
+}
